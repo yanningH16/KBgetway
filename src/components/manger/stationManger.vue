4025350957fb4391cd570d25a6d5d2831ff1f3f3
yanglx <1071126434@qq.com>
2018-02-08 15:53:02 +0800

    fenzhanguanli

--- src/components/manger/stationManger.vue
+++ src/components/manger/stationManger.vue
@@ -52,6 +52,7 @@
               <el-dropdown-menu slot="dropdown">
                 <el-dropdown-item :command="[scope.row,1]">编辑</el-dropdown-item>
                 <el-dropdown-item :command="[scope.row,3]">扣除金额</el-dropdown-item>
+                <el-dropdown-item :command="[scope.row,4]">重置密码</el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
           </template>
@@ -95,6 +96,15 @@
         <div class="buttons" style="text-align:center;margin-top:40px;">
           <span class="btn-b" style="margin-right:10px;" @click="deleMoneyObj.show = false">取消</span>
           <span class="btn" @click="sureToDele">确定</span>
+        </div>
+      </el-dialog>
+      <el-dialog title="重置密码" :append-to-body="true" :visible.sync="resetPassObj.show" width="600px" top="25vh">
+        <div class="cont" style="text-align:center;">
+          <span style="font-size:18px;font-wight:bold;">你确定要重置该用户的密码吗?请谨慎操作</span>
+        </div>
+        <div class="buttons" style="text-align:center;margin-top:40px;">
+          <span class="btn-b" style="margin-right:10px;" @click="resetPassObj.show = false">取消</span>
+          <span class="btn" @click="sureToReset">确定</span>
         </div>
       </el-dialog>
       <el-dialog :title="stationSetObj.type==0 ? '编辑分站' : '添加分站'" :append-to-body="true" :visible.sync="stationSetObj.show" width="600px" top="5vh">
@@ -197,6 +207,9 @@ export default {
         show: false,
         common: '',
         money: ''
+      },
+      resetPassObj: {
+        show: false
       },
       stationSetObj: {
         show: false,
@@ -349,9 +362,35 @@ export default {
         }
         this.stationSetObj.show = true
       } else if (command[1] === 3) { // 扣除金额
-        this.deleMoneyObj.show = true
         this.deleMoneyObj.row = command[0]
+        this.deleMoneyObj.show = true
+      } else if (command[1] === 4) { // 重置密码
+        this.resetPassObj.row = command[0]
+        this.resetPassObj.show = true
       }
+    },
+    // 确认重置密码
+    sureToReset () {
+      this.$ajax.post('/api/substation/resetPwd', {
+        channenlAccountId: this.userInfo.channelAccountId,
+        substationAccountId: this.resetPassObj.row.adminAccountId
+      }).then((data) => {
+        if (data.data.code === '200') {
+          this.$message({
+            message: '操作成功',
+            type: 'success'
+          })
+          this.getList()
+          this.resetPassObj.show = false
+        } else {
+          this.$message({
+            message: data.data.message,
+            type: 'warning'
+          })
+        }
+      }).catch(() => {
+        this.$message.error('服务器错误！')
+      })
     },
     setList (data) {
       this.tableData = data
